4977b7c4c88a27b83ef996049928e13fe88b98bc
Alex Nickolay <a.nickolay@gmail.com>
2022-01-25 13:32:16 +0100

    add blueprint APIs

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import time
 import uuid
 import os
 from flask import Flask
+from blueprints.endpoints import blueprint as endpoint
+
 
 
 # Get environment variables
@@ -33,13 +35,16 @@ def put_request(message):
 
 result = put_request("hello world from ec2")
 
-print(json.dumps(result))
-
 app = Flask(__name__)
 
-@app.route('/api/hello_world')
-def hello_world():
-    return 'Hello, World!'
+#@app.route('/api/hello_world')
+#def hello_world():
+#    return 'Hello, World!'
+
+app.config['RESTPLUS_MASK_SWAGGER'] = False
+
+app.register_blueprint(endpoint)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
---
+++ blueprints/endpoints/__init__.py
@@ -0,0 +1,16 @@
+
+from flask import Blueprint
+from flask_restplus import Api
+from blueprints.endpoints.hello_world import namespace as hello_world_ns
+
+blueprint = Blueprint('api', __name__, url_prefix='/api')
+
+api_extension = Api(
+    blueprint,
+    title='The API Demo',
+    version='1.0',
+    description='Application to demonstrate an API with DynamoDB',
+    doc='/doc'
+)
+
+api_extension.add_namespace(hello_world_ns)
